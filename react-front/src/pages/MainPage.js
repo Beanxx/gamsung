b260d3a5055e41b8ea9bd0caf457f97d8b7332ab
beanxx <tnqls991211@naver.com>
2021-06-05 06:11:14 +0900

    메인 화면 이미지 영역 & 검색바 추가

--- react-front/src/pages/MainPage.js
+++ react-front/src/pages/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/common/Header';
+import Search from "../components/common/Search";
 
 
 const MainPage = () => {
   return (
       <>
           <Header/>
+          <Search/>
       </>
   );
 };
