e5b7fa29ff5f3e3e3832424670bb5a6ec1f3cd8d
blueknarr <blueknarr@gmail.com>
2021-06-11 00:05:03 +0900

    회원 가입 api 완료

---
+++ app/views/api.py
@@ -0,0 +1,26 @@
+import datetime
+import hashlib
+import json
+
+import jwt
+from flask import Blueprint, jsonify, request
+from app import db
+
+bp = Blueprint(
+    'api',
+    __name__,
+    url_prefix='/api'
+)
+
+
+@bp.route('/register', methods=['POST'])
+def register():
+    data = json.loads(request.data)
+    id = data['id']
+    pw = data['pw']
+    place = data['place']
+
+    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
+    db.users.insert_one({'id': id, 'pw': pw_hash, 'place': place})
+
+    return jsonify({'result': 'success'})
