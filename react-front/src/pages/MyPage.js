9550d99275749585ab91fb71307dcbb8170128d5
CHOMISOL <lillit0728@gmail.com>
2021-06-12 11:53:09 +0900

    0612_로컬에서 작업 업데이트

--- react-front/src/pages/MyPage.js
+++ react-front/src/pages/MyPage.js
@@ -1,10 +1,22 @@
 import React from "react";
+import TabList from "../components/mypage/tabList";
+import MyInfo from "../components/mypage/infomation";
+import styled from "styled-components";
+
+const MypageDiv = styled.div`
+  position: relative;
+  width: 100%;
+  height:800px;
+  background: yellow;
+`;
+
 
 function MyPage() {
     return (
-        <>
-            <h1>MyPage</h1>
-        </>
+        <MypageDiv>
+            <TabList/>
+            <MyInfo/>
+        </MypageDiv>
     );
 }
 
