0fc090d335cf7c929a591403d197f534bd974931
beanxx <tnqls991211@naver.com>
2021-06-18 18:50:26 +0900

    main page: 지역 선택 & 테마 선택 박스 추가

--- react-front/src/components/mainpage/Place.js
+++ react-front/src/components/mainpage/Place.js
@@ -6,10 +6,10 @@ const PlaceBlock = styled.div`
   display: flex;
   width: 70%;
   margin: 0 auto;
-  height: 170px;
+  height: 190px;
   background: #f8f9fa;
   flex-direction: column;
-  padding:10px;
+  padding:5px;
   padding-left: 30px;
   padding-right: 30px;
 `;
@@ -21,25 +21,38 @@ const Place = () => {
             <PlaceBlock>
                 <p className="place-title">원하는 지역 둘러보기</p>
                 <div className="place-boxall">
-                    <div className="place-box">
+                    <div className="place-box"
+                    >
                         <img className="place-pic"
-                             src="https://cdn.pixabay.com/photo/2019/04/29/07/33/streets-4165342_960_720.jpg"/>
-                        <p>홍대</p>
+                             src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20201106_135%2F1604643044365HUfrK_JPEG%2Fupload_02e9864863df7bc7f30f779feda84a68.jpeg"/>
+                        <div className="place-location">
+                            <p className="place-name">홍대</p>
+                            <p className="place-subname">서울 마포구</p>
+                        </div>
                     </div>
                     <div className="place-box">
                         <img className="place-pic"
-                             src="https://cdn.pixabay.com/photo/2019/04/29/07/33/streets-4165342_960_720.jpg"/>
-                        <p>상수</p>
+                             src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20210228_261%2F16144402497217QKxk_JPEG%2Fupload_610a7c259b07b83c03ce8cc948fdefe8.jpeg"/>
+                        <div className="place-location">
+                            <p className="place-name">상수</p>
+                            <p className="place-subname">서울 마포구</p>
+                        </div>
                     </div>
                     <div className="place-box">
                         <img className="place-pic"
-                             src="https://cdn.pixabay.com/photo/2019/04/29/07/33/streets-4165342_960_720.jpg"/>
-                        <p>연남</p>
+                             src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA0MTJfMjI5%2FMDAxNjE4MjAwMjg3NzQ5.vHP93Xgg5-1SyEhYux_BYTY-88hSKaPmdp024SunSW8g.nj-dPlzOanBZCJT-u2ajkB8k_pC6TkR-1uc1T34b_xEg.JPEG.epiiplus1-0%2FIMG_9099.JPG"/>
+                        <div className="place-location">
+                            <p className="place-name">연남</p>
+                            <p className="place-subname">서울 마포구</p>
+                        </div>
                     </div>
                     <div className="place-box">
                         <img className="place-pic"
-                             src="https://cdn.pixabay.com/photo/2019/04/29/07/33/streets-4165342_960_720.jpg"/>
-                        <p>망원</p>
+                             src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20210523_95%2F1621763920687HqxlF_JPEG%2Fupload_973bd2f8381d3a2d0af84580a6f7cb6d.jpg"/>
+                        <div className="place-location">
+                            <p className="place-name">망원</p>
+                            <p className="place-subname">서울 마포구</p>
+                        </div>
                     </div>
                 </div>
             </PlaceBlock>
